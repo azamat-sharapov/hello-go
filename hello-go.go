e7c283279596b80d7705ccdad557a7ae3d779bd8
Azamat <azamat.jobcontact@gmail.com>
2016-09-02 23:44:33 +0700

    Integrate postgres

--- hello-go.go
+++ hello-go.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 	"html/template"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+var db *sqlx.DB
+
+type Person struct {
+	Name string
+}
+
+func webServerHandler(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("templates/index.html")
 
 	if err != nil {
@@ -30,12 +38,49 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "")
 }
 
+func connectDb() error {
+	var err error
+	var schema = `
+	CREATE TABLE IF NOT EXISTS people (
+		name TEXT
+	)
+	`
+
+	db, err = sqlx.Connect("postgres", "user=dev dbname=hello-go sslmode=disable")
+
+	if err == nil {
+		db.MustExec(schema)
+	}
+
+	return err
+}
+
+func saveName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	_, err := db.NamedExec("INSERT INTO people (name) VALUES (:name)", Person{r.PostForm.Get("name")})
+
+	if err != nil {
+		showError(w, err)
+		return
+	}
+
+	fmt.Fprintf(w, "ok")
+}
+
 func showError(w http.ResponseWriter, err error) {
 	fmt.Println(err)
 	http.Error(w, "Internal Server Error", 500)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	dbErr := connectDb()
+
+	if dbErr != nil {
+		fmt.Println(dbErr)
+		return
+	}
+
+	http.HandleFunc("/", webServerHandler)
+	http.HandleFunc("/save-my-name", saveName)
 	http.ListenAndServe(":8182", nil)
 }
