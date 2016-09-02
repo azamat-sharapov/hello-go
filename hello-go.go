70a7b2882d1b394192d088bf64eef1e3aace8b15
Azamat <azamat.jobcontact@gmail.com>
2016-09-02 20:44:09 +0700

    Integrate templating

--- hello-go.go
+++ hello-go.go
@@ -2,11 +2,31 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello go!")
+	t, err := template.ParseFiles("templates/index.html")
+
+	if err != nil {
+		showError(w, err)
+		return
+	}
+
+	err = t.Execute(w, nil)
+
+	if err != nil {
+		showError(w, err)
+		return
+	}
+
+	fmt.Fprintf(w, "")
+}
+
+func showError(w http.ResponseWriter, err error) {
+	fmt.Println(err)
+	http.Error(w, "Internal Server Error", 500)
 }
 
 func main() {
